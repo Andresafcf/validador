c2043c496090d86d8b212019ed10e84acd8e5347
Andresafcf <61529321+Andresafcf@users.noreply.github.com>
2020-03-27 18:30:06 -0500

    Add files via upload

---
+++ contraseña.py
@@ -0,0 +1,10 @@
+import sys
+import re
+contraseña= sys.argv[1]
+validar=re.search("^[A-Z][0-9]{3}[a-z]+[\W]{3}$", contraseña)
+
+if validar!=None:
+   print("la contraseña  es valida")
+else:
+    print("la contraseña no es valida")
+
